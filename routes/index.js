1ad68b3d32a446f4eb90faad2e3d5108b32bfb14
diegosdt <diegotua066@gmail.com>
2025-10-20 18:34:55 +0200

    intento

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+app.get('/', (req, res) => {
+  res.send('API RESTful activa. Visita /about o /api-docs.');
+});
+
+
 
 module.exports = router;
