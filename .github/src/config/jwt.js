4db8c201744f0a6a6f49457ba563f3cc8367a445
diegosdt <diegotua066@gmail.com>
2025-12-13 20:23:39 +0100

    tercer intento

--- .github/src/config/jwt.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  secret: process.env.JWT_SECRET || 'clave_super_secreta',
-  expiresIn: '1h',
-};
--- .github/src/services/payment/paymentStrategy.js
+++
@@ -1,8 +0,0 @@
-class PaymentStrategy {
-  // paymentDetails: object
-  async processPayment(paymentDetails) {
-    throw new Error('processPayment not implemented');
-  }
-}
-
-module.exports = PaymentStrategy;
